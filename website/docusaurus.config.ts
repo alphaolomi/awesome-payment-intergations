f505df19b6d2f78d853babc78470d983b297361a
Alpha Olomi <alphaolomi@gmail.com>
2026-03-15 12:01:43 +0300

    fix: Update Docusaurus configuration for GitHub Pages deployment
    - Changed the site URL to 'https://alphaolomi.github.io' for proper deployment.
    - Updated the base URL to '/awesome-payment-intergations/' to reflect the project's structure.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -13,10 +13,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://alphaolomi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/awesome-payment-intergations/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
